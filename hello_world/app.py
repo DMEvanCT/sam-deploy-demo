28d76303fbe8653c2a6af90885efa0f26d382ed8
Evan Haston <evan.haston@slalom.com>
2022-07-13 09:27:02 -0400

    feature setting post

--- hello_world/app.py
+++ hello_world/app.py
@@ -4,26 +4,9 @@ import json
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    body = json.loads(event["body"])
+    name = body["name"]
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
 
     # try:
     #     ip = requests.get("http://checkip.amazonaws.com/")
@@ -37,7 +20,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world",
+                "message": name,
                 # "location": ip.text.replace("\n", "")
             }
         ),
